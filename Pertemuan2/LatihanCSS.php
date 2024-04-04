3357122c59219903541cd1cb6aecc71bbc4295f3
Ardiansyah <164286651+ArdiansyahRA@users.noreply.github.com>
2024-04-05 00:19:30 +0700

    Pertemuan 2

---
+++ Pertemuan2/LatihanCSS.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="mycss.css">
+</head>
+<body>
+
+<h1>My First CSS Example</h1>
+<p>This is a paragraph.</p>
+<p id="pertama"> Ardiansyah Ryan </p>
+<p id="kedua"> 22410100054 </p>
+
+</body>
+</html>
---
+++ Pertemuan2/Tugas21.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>Tugas21.php</title>
+    <link rel="stylesheet" href="mycss2.css">
+</head>
+<body>
+<form id="Tambah">
+<br></br>
+    <label for="kode">Kode:</label><br>
+    <input type="text" id="kode" name="kode"><br>
+    
+    <label for="nama">Nama:</label><br>
+    <input type="text" id="nama" name="nama"><br>
+    
+    <label for="satuan">Satuan:</label><br>
+    <input type="text" id="satuan" name="satuan"><br>
+    
+    <label for="beli">Beli:</label><br>
+    <input type="text" id="beli" name="beli"><br>
+    
+    <label for="jual">Jual:</label><br>
+    <input type="text" id="jual" name="jual"><br>
+    
+    <br>
+    <a href="Tugas2.php" target="_blank"><input type="button" name="save" value="Save"></a>
+    <a href="Tugas2.php" target="_blank"><input type="button" name="cancel" value="Cancel"></a>
+    
+</form> 
+</body>
+
+     </html>
