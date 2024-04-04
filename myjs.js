73b30227ea97c763320c7d654355d752a9aee438
Ardiansyah <164286651+ArdiansyahRA@users.noreply.github.com>
2024-04-05 00:24:32 +0700

    Delete myjs.js

--- myjs.js
+++
@@ -1,9 +0,0 @@
-function myfungsi1()
-{
-    document.getElementById("isi1").innerHTML = "Change value element no 1";
-}
-
-function myfungsi2()
-{
-    document.getElementById("isi1").innerHTML = "Change value element no 2";
-}
