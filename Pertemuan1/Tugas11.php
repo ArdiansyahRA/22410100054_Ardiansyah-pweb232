3063f36af77c410344846fda5ee3dde19d81bf4b
Ardiansyah <164286651+ArdiansyahRA@users.noreply.github.com>
2024-04-05 00:20:47 +0700

    Pertemuan 1

---
+++ Pertemuan1/Tugas11.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>Tugas11.php</title>
+</head>
+<body>
+<form>
+<br></br>
+    <label for="kode">Kode:</label><br>
+    <input type="text" id="kode" name="kode"><br>
+    
+    <label for="nama">Nama:</label><br>
+    <input type="text" id="nama" name="nama"><br>
+    
+    <label for="satuan">Satuan:</label><br>
+    <input type="text" id="satuan" name="satuan"><br>
+    
+    <label for="beli">Beli:</label><br>
+    <input type="text" id="beli" name="beli"><br>
+    
+    <label for="jual">Jual:</label><br>
+    <input type="text" id="jual" name="jual"><br>
+    
+    <br>
+    <a href="Tugas1.php" target="_blank"><input type="button" name="save" value="Save"></a>
+    <a href="Tugas1.php" target="_blank"><input type="button" name="cancel" value="Cancel"></a>
+    
+</form> 
+</body>
+
+     </html>
